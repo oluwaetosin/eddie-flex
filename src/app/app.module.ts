fc378e1f5e2529b5e76bdd0c529144c42f47f914
Tosin Omotayo <tosin.omotayo@fintraksoftware.com>
2022-03-23 10:21:11 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent,
+    FooterComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
